6b48f62a4c7b0ae97744d6e2336af2c795112cc6
Kevin Zhuo <kevinzhuo1996@gmail.com>
2019-04-14 02:12:47 +0800

    Basic FPS control

---
+++ Assets/Scripts/FPS_control.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPS_control : MonoBehaviour
+{
+    #region Property
+
+    [SerializeField][Header("Movement speed")]
+    private float speed = 1f;
+
+    private Rigidbody rb;
+    private float sum;
+
+    #endregion
+
+    #region Unity 
+
+    private void OnEnable()
+    {
+        rb = GetComponent<Rigidbody>();
+        sum = 0.001f;
+    }
+
+    private void Update()
+    {
+        sum = Mathf.Sqrt(Input.GetAxis("Horizontal") * Input.GetAxis("Horizontal") + Input.GetAxis("Vertical") * Input.GetAxis("Vertical")) + 0.001f;
+        //Debug.Log(sum.ToString());
+
+        // maybe use velocity = max(0.5, calculate speed)
+        rb.velocity = Input.GetAxis("Horizontal") / sum * speed * transform.right + Input.GetAxis("Vertical") / sum * speed * transform.forward;
+    }
+
+    #endregion
+}
