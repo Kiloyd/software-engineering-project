69757584e7cfafeb290a563102b82539c95423ef
Jimmy <k.c.lai@outlook.com>
2019-04-18 17:00:03 +0800

    Add pellet prefab

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    #region Property
+
+    // the highest and lowest angle that player can adjust.
+    [SerializeField][Header("the highest and lowest angle")]
+    private float maximumX;
+    [SerializeField]
+    private float minimumX;
+
+    // mouse sensitivity, X and Y means the axis of the screen. 
+    [SerializeField][Header("Mouse sensitivity")]
+    private float sensitivityX;
+    [SerializeField]
+    private float sensitivityY;
+
+    // head rotate X axis, body rotate Y axis.
+    [SerializeField][Header("transform that do the rotation")]
+    private Transform head;
+    [SerializeField]
+    private Transform body;
+
+    // rotation axis for X and Y axis. Remind that they're different to sensitivities way.
+    private float rotateX;
+    private float rotateY;
+
+    #endregion
+
+    #region Unity 
+
+    private void OnEnable()
+    {
+        rotateX = 0f;
+        rotateY = 0f;
+    }
+
+    private void Update()
+    {
+        rotateY += Input.GetAxis("Mouse X") * sensitivityX;
+        rotateX -= Input.GetAxis("Mouse Y") * sensitivityY;
+
+        // only clamp the X axis rotation, no need for Y axis.
+        rotateX = Mathf.Clamp(rotateX, minimumX, maximumX);
+
+        head.localEulerAngles = new Vector3(rotateX, 0, 0);
+        body.localEulerAngles = new Vector3(0, rotateY, 0);
+    }
+
+    #endregion
+
+}
---
+++ Assets/Scripts/FpsController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FpsController : MonoBehaviour
+{
+    #region Property
+
+    [SerializeField][Header("Movement speed")]
+    private float speed = 1f;
+
+    private Rigidbody rb;
+    private float sum;
+
+    #endregion
+
+    #region Unity 
+
+    private void OnEnable()
+    {
+        rb = GetComponent<Rigidbody>();
+        sum = 0.001f;
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        sum = Mathf.Sqrt(Input.GetAxis("Horizontal") * Input.GetAxis("Horizontal") + Input.GetAxis("Vertical") * Input.GetAxis("Vertical")) + 0.001f;
+        //Debug.Log(sum.ToString());
+
+        // maybe use velocity = max(0.5, calculate speed)
+        rb.velocity = Input.GetAxis("Horizontal") / sum * speed * transform.right + Input.GetAxis("Vertical") / sum * speed * transform.forward;
+    }
+
+    #endregion
+}
