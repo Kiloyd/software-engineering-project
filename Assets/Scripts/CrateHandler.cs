adb411cd5b01cf8dab15629d44946d3df6d939a8
Jimmy <k.c.lai@outlook.com>
2019-05-12 06:18:02 +0800

    Add stations and crates Update level handler

---
+++ Assets/Scripts/CrateHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateHandler : MonoBehaviour
+{
+    [SerializeField]
+    private float dropVelocity;
+
+    private bool isLanded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isLanded = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isLanded == false) {
+            transform.Translate(Vector3.down * Time.deltaTime * dropVelocity);
+
+            Vector3 tempPos = transform.position;
+            if((tempPos.y - transform.localScale.y / 2) < 0) {
+                isLanded = true;
+                transform.localPosition = new Vector3(tempPos.x, transform.localScale.y / 2, tempPos.z);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/StationHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationHandler : MonoBehaviour
+{
+    [SerializeField]
+    private float crateInitialHeight;
+
+    [SerializeField]
+    private GameObject cratePrefab;
+
+    private GameObject levelObject; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelObject = GameObject.Find("Level");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnInteract() {
+        Debug.Log("Station interacted");
+
+        GameObject cratesObject = GameObject.Find("Crates");
+
+        GameObject tempObject = Instantiate(cratePrefab);
+        tempObject.transform.parent = cratesObject.transform;
+
+        Vector3 tempPos = transform.position;
+        tempObject.transform.localPosition = new Vector3(tempPos.x, crateInitialHeight, tempPos.z);
+
+        cratesObject.transform.localScale = new Vector3(1, 1, 1);
+
+        Destroy(this.gameObject);
+    }
+}
